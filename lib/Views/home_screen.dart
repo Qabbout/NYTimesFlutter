60b1330f85779b126a0e433c44b92828638dbe80
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-22 22:24:01 +0200

    added hoem screen view

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:nyt_articles/Constants/theme_const.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeConstant.themData,
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(),
+    );
   }
 }
