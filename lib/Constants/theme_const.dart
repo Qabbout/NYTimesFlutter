c8fa37aa045e09aa06e53454befd949641297c40
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-22 22:23:49 +0200

    added theme const

---
+++ lib/Constants/theme_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:nyt_articles/utils/color_to_material.dart';
+
+class ThemeConstant {
+  static final themData = ThemeData(
+    primarySwatch: ColorToMaterialColor.getgetMaterialColorFrom(
+        const Color.fromRGBO(55, 224, 187, 1)),
+  );
+}
