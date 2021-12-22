0a7369e65794f7f0a38a0a2d42d8c6aba32c7f35
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-23 00:43:48 +0200

    added article item view

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -21,6 +21,48 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         drawer: const Drawer(),
+        body: ListView.builder(
+          itemCount: 30,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                top: 8.0,
+                left: 8.0,
+                bottom: 8.0,
+              ),
+              child: ListTile(
+                leading: const Padding(
+                  padding: EdgeInsets.only(
+                    top: 8.0,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                  ),
+                ),
+                title: Padding(
+                  padding: const EdgeInsets.only(top: 6.0, bottom: 10.0),
+                  child: Text(
+                    "Title",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                subtitle: Stack(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 16.0),
+                      child: Text(
+                          "subtitleeeeeeeeeeeeeeessssseeeeeesseeeeeeeeeeeeeeeeeeeessssss"),
+                    ),
+                    Positioned(right: 0, bottom: 0, child: Text("Date")),
+                  ],
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.keyboard_arrow_right_rounded)),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
