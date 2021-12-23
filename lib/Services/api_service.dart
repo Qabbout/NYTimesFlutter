b1cb5dd4e22f2ddf80b193a56b4ae9cdd36319ad
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-23 10:19:53 +0200

    added api service

---
+++ lib/Services/api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:nyt_articles/Models/most_popular.dart';
+import 'package:nyt_articles/Services/network_service.dart';
+
+abstract class APIService {
+  Future<MostPopular> getArticles(String section, String period);
+}
+
+class NYTimesAPIService implements APIService {
+  NYTimesAPIService._privateConstructor();
+
+  static final NYTimesAPIService _instance =
+      NYTimesAPIService._privateConstructor();
+
+  static NYTimesAPIService get instance => _instance;
+  static var urlSection = "all-sections";
+  static var urlPeriod = "7";
+  final url =
+      "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/$urlSection/$urlPeriod.json?api-key=sample-key";
+
+  @override
+  Future<MostPopular> getArticles(String section, String period) async {
+    urlSection = section;
+    urlPeriod = period;
+    final String json;
+    try {
+      json = await NetworkService.instance.get(url);
+    } catch (e) {
+      return Future.error(e);
+    }
+    return MostPopular.fromJson(jsonDecode(json));
+  }
+}
