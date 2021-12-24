d417810da6cd4ea26b6ac667fef4afafecfc850b
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-24 19:24:19 +0200

    added article details screen test

--- test/Views/article_details_screen_test.dart
+++ test/Views/article_details_screen_test.dart
@@ -1,8 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nyt_articles/Models/result.dart';
+import 'package:nyt_articles/Views/article_details_screen.dart';
+
+import '../Fixtures/fixture_reader.dart';
 
 void main() {
+  final articleResult = Result.fromRawJson(fixture('result.json'));
   testWidgets(
-    "test description",
-    (WidgetTester tester) async {},
+    "should have an appbar with title description",
+    (WidgetTester tester) async {
+      //arrange
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ArticleDetailsScreen(
+            articleResult: articleResult,
+          ),
+        ),
+      );
+
+      //act
+      final appBarTitle = find.text(articleResult.title);
+
+      //assert
+      expect(appBarTitle, findsOneWidget);
+    },
+  );
+  testWidgets(
+    "should have an center text with abstract description",
+    (WidgetTester tester) async {
+      //arrange
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ArticleDetailsScreen(
+            articleResult: articleResult,
+          ),
+        ),
+      );
+
+      //act
+      final appBarTitle = find.text(articleResult.resultAbstract);
+
+      //assert
+      expect(appBarTitle, findsOneWidget);
+    },
   );
 }
