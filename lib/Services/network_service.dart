ab0643e02c2d75203b08f507702cb63fd787a924
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-24 17:02:06 +0200

    added try catch to uri parse and client

--- lib/Services/network_service.dart
+++ lib/Services/network_service.dart
@@ -8,9 +8,17 @@ class NetworkService {
   static final NetworkService _instance = NetworkService._privateConstructor();
 
   static NetworkService get instance => _instance;
+
+  static http.Client client = http.Client();
   Future<String> get(String url) async {
-    var urlRequest = Uri.parse(url);
-    var response = await http.get(urlRequest);
+    Uri urlRequest;
+    try {
+      urlRequest = Uri.parse(url);
+    } catch (e) {
+      return e.toString();
+    }
+
+    var response = await client.get(urlRequest);
     checkAndThrowError(response);
     return response.body;
   }
