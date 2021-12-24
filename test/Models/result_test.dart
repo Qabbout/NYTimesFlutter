5a7b9a0a3b289a35821074023bde97547534f46d
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-24 17:04:44 +0200

    added result model test

---
+++ test/Models/result_test.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nyt_articles/Models/result.dart';
+
+import '../fixtures/fixture_reader.dart';
+
+void main() {
+  final tResult = Result(
+      adxKeywords: "a",
+      byline: "as",
+      id: 1,
+      nytdsection: "das",
+      publishedDate: DateTime.parse("2020-10-10"),
+      resultAbstract: "abstract",
+      section: "section",
+      title: "title",
+      updated: DateTime.parse("2020-10-11"),
+      uri: "uri",
+      url: "url");
+
+  test('should be a subclass of Result Model', () async {
+    expect(tResult, isInstanceOf<Result>());
+  });
+  test('should return a valid Result model', () async {
+    // arrange
+    final Map<String, dynamic> jsonMap = json.decode(fixture('result.json'));
+
+    // act
+    final result = Result.fromJson(jsonMap);
+
+    // assert
+    expect(result.toString(), tResult.toString());
+  });
+}
