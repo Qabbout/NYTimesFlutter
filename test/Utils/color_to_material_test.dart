b1de263c6ab51c71beb2b0613af5ee234da5ed24
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-12-24 17:05:02 +0200

    added color to material test

--- test/Utils/color_to_material_test.dart
+++ test/Utils/color_to_material_test.dart
@@ -4,9 +4,13 @@ import 'package:nyt_articles/Utils/color_to_material.dart';
 
 void main() {
   test('Color should be converted to MaterialColor', () {
+    //arrange
     const Color color = Color.fromRGBO(55, 224, 187, 1);
+
+    //act
     final materiaColol = ColorToMaterialColor.getgetMaterialColorFrom(color);
 
+    //assert
     expect(materiaColol, isInstanceOf<MaterialColor>());
   });
 }
